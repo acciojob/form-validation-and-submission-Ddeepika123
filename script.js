c8cd65156ebfb5f09fc7bbd11cfa32cc9cf8a037
Dpka Singh <112195696+Ddeepika123@users.noreply.github.com>
2026-09-22 00:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const form = document.getElementById("myForm");
+const terms = document.getElementById("terms");
+const submit = document.getElementById("submit");
+
+terms.addEventListener("change", function () {
+
+    if (terms.checked) {
+        submit.disabled = false;
+    } else {
+        submit.disabled = true;
+    }
+
+});
+
+form.addEventListener("submit", function (event) {
+
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        form.reportValidity();
+    }
+
+});
